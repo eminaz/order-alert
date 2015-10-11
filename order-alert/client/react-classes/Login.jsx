49414b7f88e66ad2c430ef3c8dee805292d60f40
Lisa Wong <miss.lisa19@hotmail.com>
2015-10-10 23:35:04 -0700

    added login and register button

--- order-alert/client/react-classes/Login.jsx
+++ order-alert/client/react-classes/Login.jsx
@@ -2,15 +2,18 @@ Login = React.createClass({
 	render() {
 		return (
 
-      <div>
-      <h1> Login</h1>
-        <label htmlfor="username">Enter your username: </label>
-        <input type="text" /> 
-        <br/>       
-        <label htmlfor="password">Enter your password: </label>
-        <input type="text" />
-      </div>
-
-		);
+			<div id='login-page'>
+				<h1> Login</h1>
+				<label htmlfor="username">Enter your ID: </label>
+				<input type="text" /> 
+				<br />       
+				<label htmlfor="password">Enter your password: </label>
+				<input type="text" />
+				<br />
+				<button className="btn btn-info" type="button">Login</button>
+				<button className="btn btn-warning" type="button"> Register</button>
+				<br />
+			</div>
+		)
 	}
 })
