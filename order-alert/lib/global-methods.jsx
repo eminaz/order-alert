51e52cee882c9d6d8f48f86a2917b5fb10685919
Eric Kim <bosung90@gmail.com>
2015-10-10 19:58:01 -0700

    meteor methods we need, still need to populate the parameters and logic inside

---
+++ order-alert/lib/global-methods.jsx
@@ -0,0 +1,36 @@
+Meteor.methods({
+
+	// Use Meteor methods if you need to call any sever method from the client
+
+	addOrder() {
+	},
+
+	removeOrder() {
+	},
+
+	addDish() {
+	},
+
+	updateDish() {
+	},
+
+	removeDish() {
+	},
+
+	addCategory() {
+	},
+
+	renameCategory() {
+	},
+
+	removeCategory() {
+	},
+
+	archiveOrder() {
+
+	},
+
+	beepUser() {
+	},
+
+});
