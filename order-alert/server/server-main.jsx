0a293d7c11020bbcac6116036d5f5d1552d1a4b5
Eric Kim <bosung90@gmail.com>
2015-10-10 19:45:09 -0700

    added a server-side meteor method so client can become a owner

--- order-alert/server/server-main.jsx
+++ order-alert/server/server-main.jsx
@@ -14,3 +14,18 @@ Meteor.publish("orders", function () {
 	const user_id = Meteor.users() // Same as Meteor.users.findOne(Meteor.userId())
 	return Orders.find({})
 })
+
+Meteor.methods({
+
+	// Example Meteor method
+	becomeOwner(secretKey) {(
+		if (secretKey != 'markIsCool') {
+			Meteor.throws('Wrong password!')
+		}
+			
+		// Make user owner if they are not owner yet
+		if (Owners.find({user_id: Meteor.userId()}).count() == 0) {
+			Owners.insert({user_id: Meteor.userId()})
+		}
+	}
+});
