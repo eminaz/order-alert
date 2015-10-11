f873ee2f3a5f8c74e7316281e9003fa1ab3cef7f
Eric Kim <bosung90@gmail.com>
2015-10-11 14:13:13 -0700

    hook up Orders Collection with CounterMode

--- order-alert/client/iron-router.jsx
+++ order-alert/client/iron-router.jsx
@@ -1,41 +1,37 @@
 Router.configure({
   layoutTemplate: 'layout'
-});
+})
 
 Router.onAfterAction(function() {
-  this.render('sideMenu');
-});
+  this.render('sideMenu')
+})
 
 Router.route('/', function() {
-	this.render('sideMenu');
-});
+	this.render('sideMenu')
+})
 
 Router.route('/edit-menu', function() {
-	Session.set('reactClass', 'EditMenu');
-});
+	Session.set('reactClass', 'EditMenu')
+})
 
 Router.route('/order', function() {
-	Session.set('reactClass', 'Order');
-});
+	Session.set('reactClass', 'Order')
+})
 Router.route('/counter-mode', function() {
-	Session.set('reactClass', 'CounterMode');
-});
-
-
-
-
-
+	Meteor.subscribe('orders')
+	Session.set('reactClass', 'CounterMode')
+})
 
 Template.layout.rendered = function() {
-	React.render(<Settings />, document.getElementById('settingsReact'));
+	React.render(<Settings />, document.getElementById('settingsReact'))
 	Tracker.autorun(function() {
-		var reactClass = Session.get('reactClass');
+		var reactClass = Session.get('reactClass')
 		if(reactClass === 'EditMenu') {
-			React.render(<EditMenu />, document.getElementById('mainContentReact'));
+			React.render(<EditMenu />, document.getElementById('mainContentReact'))
 		}
 		if(reactClass === 'CounterMode') {
-			React.render(<CounterMode />, document.getElementById('mainContentReact'));
+			React.render(<CounterMode />, document.getElementById('mainContentReact'))
 		}
-	});
+	})
 }
 
