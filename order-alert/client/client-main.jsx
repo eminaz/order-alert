b297c13e8593e103f53aed2bfe5dc1da3dede7c6
Eric Kim <bosung90@gmail.com>
2015-10-10 22:40:35 -0700

    remove unused files

--- order-alert/client/client-main.jsx
+++
@@ -1,5 +0,0 @@
-Meteor.startup(function () {
-	// Populate html element with Id using given React class's render method
-	React.render(<Login />, document.getElementById('login-page'))
-	React.render(<EditMenu />, document.getElementById('edit-menu-page'))
-})
